66003f59c4683a52ab9868f0ab1ca71587c8dc66
Bayezid Mostafa <hiperx1@gmail.com>
2022-12-05 18:04:58 +0600

    Put API Created for upcoming Car Data

--- index.js
+++ index.js
@@ -13,17 +13,52 @@ app.use(express.json())
 
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const run = async() => {
-    try{
+const run = async () => {
+    try {
         const carCollection = client.db('resellcar').collection('cardata');
-        app.get('/cars', async(req, res) => {
+        app.get('/cars', async (req, res) => {
             const query = {};
             const result = await carCollection.find(query).toArray();
             res.send(result);
         })
+        app.put('/sellcar', async (req, res) => {
+            const data = req.body;
+            const {
+                car_name,
+                location,
+                picture,
+                engine,
+                transmission,
+                price,
+                mileage
+            } = data;
+            const filter = {
+                car_name,
+                location,
+                picture,
+                engine,
+                transmission,
+                price,
+                mileage
+            }
+            const option = { upsert: true }
+            const updatedDock = {
+                $set: {
+                    car_name,
+                    location,
+                    picture,
+                    engine,
+                    transmission,
+                    price,
+                    mileage
+                }
+            }
+            const products = await carCollection.updateOne(filter, updatedDock, option);
+            res.send({message: 'Product Added Successfully'})
+        })
     }
-    catch{}
-    finally{}
+    catch { }
+    finally { }
 }
 
 run().catch(error => console.error(error))
