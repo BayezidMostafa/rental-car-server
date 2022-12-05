707fb068aec8c2260a5798d35a13003d1ec50ee6
Bayezid Mostafa <hiperx1@gmail.com>
2022-12-05 11:28:29 +0600

    Car API Created

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async() => {
+    try{
+        const carCollection = client.db('resellcar').collection('cardata');
+        app.get('/cars', async(req, res) => {
+            const query = {};
+            const result = await carCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    catch{}
+    finally{}
+}
+
+run().catch(error => console.error(error))
 
 app.get('/', (req, res) => {
     res.send('Server is running')
